224eca66aeea083483d4042e92f8dbc11780d79e
JhoanSV <sebastian011295@gmail.com>
2021-04-19 19:42:49 +0200

    primer commit

---
+++ JavaScript/www/src.js
@@ -0,0 +1,5 @@
+//Al declarar una variable, usar nombres en ingles, sin acentos o ñ, de ser necesario
+// solo usar _ o $ al inicio de una variable para declararla.
+let age = null;
+
+console.log(typeof age);
